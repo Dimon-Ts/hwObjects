0fc7c88c1a3efb04f99cfe370c10d801eafb8db0
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-10-06 01:46:14 +0300

    Объекты.Домашнее задание.Cделал но без сеттеров.Не понял как работают.

---
+++ src/Book.java
@@ -0,0 +1,22 @@
+public class Book {
+    private final String title;
+    private final int publishingYear;
+
+
+
+    public Book(String title, int publishingYear) {
+        this.title = title;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+
+
+}
